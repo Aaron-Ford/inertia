3ece84a433c6adef0aa5c8ee301861c360cd35c4
AaronFordSVVSD <46492513+AaronFordSVVSD@users.noreply.github.com>
2021-03-19 13:52:25 -0600

    update

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ $('.contact-us').click(function(){
 	$('.modal').fadeIn();
 	$('.modal').addClass('active');
 })
+
+$('.exit').click(function(){
+	$('.modal').fadeOut();
+	$('.modal').removeClass('active');
+})
