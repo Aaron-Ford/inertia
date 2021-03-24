0e75c4ee1929bada4231bd3b95c8f7f12d06ad34
AaronFordSVVSD <46492513+AaronFordSVVSD@users.noreply.github.com>
2021-03-24 11:06:05 -0600

    update

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 $('.contact-us').click(function(){
 	$('.modal').fadeIn();
 	$('.modal').addClass('active');
+		console.log('click');
+	$('iframe').attr("src", "https://player.vimeo.com/video/305363625?controls=1&amp;autoplay=1&amp;loop=1&amp;byline=0&amp;title=0&amp;color=afffda&amp;muted=0")
 })
 
 $('.exit').click(function(){
@@ -8,4 +10,10 @@ $('.exit').click(function(){
 	$('.modal').removeClass('active');
 })
 
-document.body.className = "visible";
+
+  AOS.init();
+
+$('.vimeo-wrapper').click(function(){
+	console.log('click');
+	$(this).attr("src", "https://player.vimeo.com/video/305363625?controls=1&amp;autoplay=1&amp;loop=1&amp;byline=0&amp;title=0&amp;color=afffda&amp;muted=0")
+})
